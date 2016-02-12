52a6d186ffa44d2a319af308b366134004c8c9a1
cwglasso <cwglasso@ncsu.edu>
2016-02-12 12:54:24 -0500

    Added object definition for lock object Created group and child layouts for key list Added internet permission to the manifest

---
+++ LoveLockClient/app/src/main/java/com/example/owner/lovelockclient/Lock.java
@@ -0,0 +1,52 @@
+package com.example.owner.lovelockclient;
+
+import java.io.Serializable;
+
+/**
+ * Object representing a lock and key
+ * Created by Corey Glasson on 2/12/2016.
+ */
+public class Lock implements Serializable {
+
+
+
+    /**
+     * The unique ID for the lock.
+     * Represents the "key" for the lock
+     */
+    protected String id;
+    /**
+     * The name of the lock
+     */
+    protected String name;
+    /**
+     * The message contained within the lock
+     */
+    protected String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+}
