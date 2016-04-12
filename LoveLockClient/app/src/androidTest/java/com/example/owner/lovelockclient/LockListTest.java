b3e92e8d94694c3bf9eb635bdadea9172315e8ac
Joseph Gregory <jcgrego3@ncsu.edu>
2016-04-12 13:49:56 -0400

    Added test to see if locks save after app closing

--- LoveLockClient/app/src/androidTest/java/com/example/owner/lovelockclient/LockListTest.java
+++ LoveLockClient/app/src/androidTest/java/com/example/owner/lovelockclient/LockListTest.java
@@ -1,6 +1,8 @@
 package com.example.owner.lovelockclient;
 
+import android.app.Activity;
 import android.app.Application;
+import android.test.ActivityInstrumentationTestCase2;
 import android.test.ApplicationTestCase;
 
 import java.util.ArrayList;
@@ -8,35 +10,42 @@ import java.util.ArrayList;
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class LockListTest extends ApplicationTestCase<Application> {
+public class LockListTest extends ActivityInstrumentationTestCase2<MainActivity> {
     private Application app;
+    private MainActivity act;
 
-    public LockListTest() {
-        super(Application.class);
-    }
+    public LockListTest() { super(MainActivity.class); }
 
     protected void setUp() throws Exception {
         super.setUp();
-        createApplication();
-        app = getApplication();
     }
 
-//    public void loadLocksTest() {
-//        createApplication();
-//
-//        LockList lockList = LockList.getInstance();
-//        //LockList.DEBUG_STORED_LOCKS_FILENAME = "app/src/main/res/testLocks";
-//        lockList.loadLocks();
-//        ArrayList<Lock> list = lockList.getList();
-//
-//        assertEquals(list.get(0).getId(), "324k");
-//        assertEquals(list.get(0).getName(), "Lock 1");
-//        assertEquals(list.get(0).getMessage(), "This is the secret message");
-//
-//        assertEquals(list.get(0).getId(), "23%#@#");
-//        assertEquals(list.get(0).getName(), "Lock 1");
-//        assertEquals(list.get(0).getMessage(), "secret message of lock 2");
-//    }
+    public void testloadLocksAfterPauseTest() {
+        LockList lockList = LockList.getInstance();
+        lockList.addLock(new Lock("324k", "Lock 1", "pass 1"));
+        lockList.addLock(new Lock("23%#@#", "Lock 2", "password2"));
+        //lockList.saveLocks();
+
+        Activity activity = getActivity();
+        activity.finish();
+
+        lockList.loadLocks();
+        LockList.getInstance().addLock(new Lock("09ykk", "Lock 3", "password#3"));
+
+        ArrayList<Lock> list = lockList.getList();
+
+        assertEquals(list.get(0).getId(), "324k");
+        assertEquals(list.get(0).getName(), "Lock 1");
+        assertEquals(list.get(0).getPassword(), "pass 1");
+
+        assertEquals(list.get(1).getId(), "23%#@#");
+        assertEquals(list.get(1).getName(), "Lock 2");
+        assertEquals(list.get(1).getPassword(), "password2");
+
+        assertEquals(list.get(list.size() - 1).getId(), "09ykk");
+        assertEquals(list.get(list.size() - 1).getName(), "Lock 3");
+        assertEquals(list.get(list.size() - 1).getPassword(), "password#3");
+    }
 
     public void testSaveLocks() {
         LockList lockList = LockList.getInstance();
